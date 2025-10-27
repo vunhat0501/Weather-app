44075dbc1caa577f459661a75aec6328e937d657
boyvotinh <LongVTQ.PTIT@gmail.com>
2025-10-27 20:55:39 +0700

    code 1

--- app/src/main/java/com/example/weatherapp/MainActivity.java
+++ app/src/main/java/com/example/weatherapp/MainActivity.java
@@ -1,6 +1,12 @@
 package com.example.weatherapp;
 
 import android.os.Bundle;
+import android.view.PixelCopy;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,19 +14,95 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.lang.reflect.Executable;
+import java.util.concurrent.Executor;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
 public class MainActivity extends AppCompatActivity {
 
     private final String API_KEY = BuildConfig.API_KEY;
+    private TextView cityNameText, temperatureText, humidityText, descriptionText, windText;
+    private ImageView wertherIcon;
+    private Button refreshButton;
+    private EditText cityNameInput;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+        cityNameText = findViewById(R.id.cityNameText);
+        temperatureText = findViewById(R.id.temperatureText);
+        humidityText = findViewById(R.id.humidityText);
+        windText = findViewById(R.id.windText);
+        descriptionText = findViewById(R.id.descriptionText);
+        wertherIcon = findViewById(R.id.weatherIcon);
+        refreshButton = findViewById(R.id.fetchWeatherButton);
+        cityNameInput = findViewById(R.id.cityNameInput);
+        refreshButton.setOnClickListener(v -> {
+            String cityName = cityNameInput.getText().toString();
+            if (!cityName.isEmpty()) {
+                FetchWeatherData(cityName);
+            } else {
+                cityNameInput.setError("Plesas enter a city name");
+            }
+        });
+        FetchWeatherData("hanoi");
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+    }
+
+    private void FetchWeatherData(String cityName) {
+        String url = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=" + API_KEY + "&units=metric";
+        ExecutorService executorService = Executors.newSingleThreadExecutor();
+        executorService.execute(() ->
+                {
+                    OkHttpClient client = new OkHttpClient();
+                    Request request = new Request.Builder().url(url).build();
+                    try {
+                        Response response = client.newCall(request).execute();
+                        String result = response.body().string();
+                        runOnUiThread(()-> updateUI(result));
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+        );
+    }
+    private  void updateUI(String result){
+        if(result != null){
+            try {
+                JSONObject jsonObject = new JSONObject(result);
+                JSONObject main = jsonObject.getJSONObject("main");
+                double temperature = main.getDouble("temp");
+                double humidity = main.getDouble("humidity");
+                double windSpeed = jsonObject.getJSONObject("wind").getDouble("speed");
+                String description = jsonObject.getJSONArray("weather").getJSONObject(0).getString("description");
+                String iconCode = jsonObject.getJSONArray("weather").getJSONObject(0).getString("icon");
+                String resourceName = "ic_" + iconCode;
+                int resId = getResources().getIdentifier(resourceName, "drawable", getPackageName());
+                wertherIcon.setImageResource(resId);
+                cityNameText.setText(jsonObject.getString("name"));
+                temperatureText.setText(String.format("%.0f", temperature));
+                humidityText.setText(String.format("%.0f%%", humidity));
+                windText.setText(String.format("%.0f Km/h", windSpeed));
+                descriptionText.setText(description);
+            } catch (JSONException e){
+                e.printStackTrace();
+            }
+        }
     }
 }
